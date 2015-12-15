38785f8700fc8955c21ba0b496bb7c4cb0273d50
Tim Bradt <tjbradt@mtu.edu>
2015-12-15 12:23:15 -0500

    Added Type2 Triangle and Gaussian Fuzzy Sets

--- src/Type1/Type1.jl
+++ src/Type1/Type1.jl
@@ -8,51 +8,54 @@ export Membershipfunction
 
 import Base: getindex
 
-getindex(mf::Membershipfunction, range::Range) = [mf[value] for value in range]
+getindex(mf::Membershipfunction, range) = [mf[value]::Float64 for value in range]
+getindex(mf::Membershipfunction, values...) = [mf[value]::Float64 for value in values]
 
 export getindex
 
 #####################
 # Membership Functions
 "Triangle Membership Function"
-type TriangleMF <: Membershipfunction
-	a::Real
-	b::Real
-	c::Real
+immutable TriangleMF <: Membershipfunction
+	a::Float64
+	b::Float64
+	c::Float64
+
+	TriangleMF(a,b,c) = a <= b <= c ? new(a,b,c) : error("Indices out of order")
 end
 export TriangleMF
 
 import Base: getindex
 function getindex(trimf::TriangleMF, value::Real)
-	(value <= trimf.a || trimf.c <= value) && return 0
-	(value == trimf.b) && return 1
+	(value <= trimf.a || trimf.c <= value) && return zero(Float64)
+	(value == trimf.b) && return one(Float64)
 
 	if trimf.a < value < trimf.b
-		slope = (1 - 0)/(trimf.b - trimf.a)
+		slope = (1.0 - 0.0)/(trimf.b - trimf.a)
 		return slope * (value - trimf.a)
 	else
-		slope = (0 - 1)/(trimf.c - trimf.b)
-		return slope * (value - trimf.b) + 1
+		slope = (0.0 - 1.0)/(trimf.c - trimf.b)
+		return (slope * (value - trimf.b) + 1.0)
 	end
 end
-
 export getindex
 
 
+
 "Trapezoid Membership Function"
-type TrapezoidMF <: Membershipfunction
-	a::Real
-	b::Real
-	c::Real
-	d::Real
+immutable TrapezoidMF <: Membershipfunction
+	a::Float64
+	b::Float64
+	c::Float64
+	d::Float64
 end
 export TrapezoidMF
 
 
 import Base: getindex
 function getindex(trapmf::TrapezoidMF, value::Real)
-	(value <= trapmf.a || trapmf.d <= value) && return 0
-	(trapmf.b < value < trapmf.c) && return 1
+	(value <= trapmf.a || trapmf.d <= value) && return zero(Float64)
+	(trapmf.b <= value <= trapmf.c) && return one(Float64)
 
 	if trapmf.a < value < trapmf.b
 		slope = (1 - 0)/(trapmf.b - trapmf.a)
@@ -63,4 +66,22 @@ function getindex(trapmf::TrapezoidMF, value::Real)
 	end
 end
 export getindex
+
+
+"Generic Membership Function"
+immutable GenericMF <: Membershipfunction
+	mf::Function
+end
+export GenericMF
+typealias GaussMF GenericMF
+GaussMF(mu,sigma) = GenericMF(x -> exp(-(x-mu).^2/(2.*sigma.^2)))
+export GaussMF
+
+import Base: getindex
+function getindex(gmf::GenericMF, value::Real)
+	gmf.mf(value)
+end
+export getindex
+
+
 #####################
--- src/Type2/Type2.jl
+++ src/Type2/Type2.jl
@@ -1,7 +1,65 @@
 
-abstract Type2FuzzySet <: FuzzySet
+abstract Type2FuzzySet <: AbstractFuzzySet
 
-type IntervalType2FuzzySet <: Type2FuzzySet
-	lowerBound::Membershipfunction
-	upperBound::Membershipfunction
+#####################
+abstract IntervalType2FuzzySet <: Type2FuzzySet
+import Base: getindex
+
+getindex(mf::IntervalType2FuzzySet, range) = [mf[value]::Tuple{Float64,Float64} for value in range]
+getindex(mf::IntervalType2FuzzySet, values...) = [mf[value]::Tuple{Float64,Float64} for value in values]
+
+export getindex
+
+#####################
+# Interval 2 Membership Functions
+immutable GaussType2 <: IntervalType2FuzzySet
+	upper::GaussMF
+	lower::GaussMF
+	muU
+	muL
+	GaussType2(muU,muL,sigmaU,sigmaL) = new(GaussMF(muU,sigmaU),GaussMF(muL,sigmaL),muU,muL)
 end
+export GaussType2
+
+import Base: getindex
+function getindex(gmf::GaussType2, value::Real)
+	if gmf.muU <= value <= (gmf.muL+gmf.muU)/2
+		mu1 = 1
+		mu2 = gmf.lower[value]
+	elseif (gmf.muL+gmf.muU)/2 < value <= gmf.muL
+		mu1 = 1
+		mu2 = gmf.upper[value]
+	else
+		mu1 = gmf.upper[value]
+		mu2 = gmf.lower[value]
+	end
+	return (max(mu1,mu2),min(mu1,mu2))
+end
+export getindex
+
+immutable TriangleType2 <: IntervalType2FuzzySet
+	lower::TriangleMF
+	upper::TriangleMF
+	# TriangleType2(muU,muL,sigmaU,sigmaL) = new(GaussMF(muU,sigmaU),GaussMF(muL,sigmaL),muU,muL)
+end
+export TriangleType2
+
+import Base: getindex
+function getindex(mf::TriangleType2, value::Real)
+	m1 = (mf.lower.b <= mf.upper.b) ? mf.lower : mf.upper
+	m2 = (mf.lower.b > mf.upper.b) ? mf.lower : mf.upper
+	if m1.b <= value <= (m1.b+m2.b)/2
+		mu1 = 1
+		mu2 = m2[value]
+	elseif (m1.b+m2.b)/2 < value <= m2.b
+		mu1 = 1
+		mu2 = m1[value]
+	else
+		mu1 = m1[value]
+		mu2 = m2[value]
+	end
+	return (max(mu1,mu2),min(mu1,mu2))
+end
+export getindex
+
+#####################
